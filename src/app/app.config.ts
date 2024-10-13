f7a539058c4a59a2edc5d7a41be53660f1266f84
hoshima <hoshima@users.noreply.github.com>
2024-10-13 17:54:40 +0900

    add environment file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,21 +8,13 @@ import {
   provideAnalytics,
   ScreenTrackingService,
 } from '@angular/fire/analytics';
+import { environment } from '../environments/environment.development';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'fir-test-2-d97f8',
-        appId: '1:95858383166:web:44bc3c87a600396077b46f',
-        storageBucket: 'fir-test-2-d97f8.appspot.com',
-        apiKey: 'AIzaSyAlhdCDDP5-0Ribo1r_Daitkqxw3Pox0WA',
-        authDomain: 'fir-test-2-d97f8.firebaseapp.com',
-        messagingSenderId: '95858383166',
-      })
-    ),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
     ScreenTrackingService,
   ],
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'fir-test-2-d97f8',
+    appId: '1:95858383166:web:44bc3c87a600396077b46f',
+    storageBucket: 'fir-test-2-d97f8.appspot.com',
+    apiKey: 'AIzaSyAlhdCDDP5-0Ribo1r_Daitkqxw3Pox0WA',
+    authDomain: 'fir-test-2-d97f8.firebaseapp.com',
+    messagingSenderId: '95858383166',
+  },
+};
